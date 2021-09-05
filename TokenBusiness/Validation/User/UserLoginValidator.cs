f7db7b5af11152442e0c0106a5d9a314069ad648
Ömer Furkan Doğruyol <omerfurkandogruyol@gmail.com>
2021-09-05 11:47:39 +0300

    User login validator added.

---
+++ TokenBusiness/Validation/User/UserLoginValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TokenEntity.Dto.User;
+
+namespace TokenBusiness.Validation.User
+{
+    public class UserLoginValidator : AbstractValidator<UserLoginDto>
+    {
+        public UserLoginValidator()
+        {
+            RuleFor(p => p.IdentityNumber)
+                .NotEmpty().WithMessage("Identity number should be filled.")
+                .Length(11, 11).WithMessage("Identity number must be exact 11 character.");
+            RuleFor(p => p.Password).NotEmpty().WithMessage("Password should be filled.");
+        }
+    }
+}
